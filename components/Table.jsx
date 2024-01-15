1dad17470d75d450f1ff5e19c3d183dbc616c486
gowdaman-dev <damangowdaman@gmail.com>
2024-01-16 00:16:48 +0530

    new1

--- components/Table.jsx
+++ components/Table.jsx
@@ -78,40 +78,42 @@ const table = () => {
   ];
   const headings = Object.keys(data[0]);
   return (
-    <div className='w-1/2 m-auto'>
-      <TableContainer component={Paper} className='rounded-xl overflow-hidden'>
-        <Table sx={{ minWidth: 200 }} aria-label="simple table">
-          <TableHead className='bg-blue-100'>
-            <TableRow>
-              {headings.map((heading) => (
-                heading !== 'id' ? (
-                  <TableCell key={heading}>{heading}</TableCell>
-                ) :
-                  null
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((value) => (
-              <TableRow key={value.id} >
-
+    <div className="w-screen h-screen grid place-items-center">
+      <div className='w-1/2 m-auto'>
+        <TableContainer component={Paper} className='rounded-x'>
+          <Table className='w-full' aria-label="simple table">
+            <TableHead className='bg-blue-100'>
+              <TableRow>
                 {headings.map((heading) => (
-                  heading === 'id' ? (
-                    null
-                  ) : heading === "Details" ? (
-                    <TableCell key={heading} align="left"><button> {value[heading]} </button></TableCell>
-
+                  heading !== 'id' ? (
+                    <TableCell className='p-0' align='center' key={heading}>{heading}</TableCell>
                   ) :
-
-                    (
-                      <TableCell key={heading} align="left"><button> {value[heading]}</button></TableCell>
-                    )
+                    null
                 ))}
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {data.map((value) => (
+                <TableRow key={value.id}  >
+
+                  {headings.map((heading) => (
+                    heading === 'id' ? (
+                      null
+                    ) : heading === "Details" ? (
+                      <TableCell key={heading} className='p-0' align="center"><button> {value[heading]} </button></TableCell>
+
+                    ) :
+
+                      (
+                        <TableCell key={heading} className='p-0' align="center"><button> {value[heading]}</button></TableCell>
+                      )
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
     </div>
   )
 }
