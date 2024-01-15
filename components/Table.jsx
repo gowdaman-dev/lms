584c9e17ca80c9a659ded1b36ab1a3e3ebcb009f
gowdaman-dev <damangowdaman@gmail.com>
2024-01-16 00:05:49 +0530

    new1

--- components/Table.jsx
+++ components/Table.jsx
@@ -1,115 +1,118 @@
-
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import React from 'react'
 
 const table = () => {
-    const data=[
-        {
-            Student_Name : 'deepath',
-            id:1,
-            Type: 'Minato',
-            Standard: "III standard",
-            Phone_Number: 123,
-            ERP_Number: 5454,
-            Status : 'Active ',
-            Details : 'View' 
+  const data = [
+    {
+      Student_Name: 'deepath',
+      id: 1,
+      Type: 'Minato',
+      Standard: "III standard",
+      Phone_Number: 123,
+      ERP_Number: 5454,
+      Status: 'Active ',
+      Details: 'View'
 
-        }, 
-          {
-            Student_Name : 'deepath',
-            id:1,
-            Type: 'Minato',
-            Standard: "III standard",
-            Phone_Number: 123,
-            ERP_Number: 5454,
-            Status : 'Active ',
-            Details : 'View' 
+    },
+    {
+      Student_Name: 'deepath',
+      id: 1,
+      Type: 'Minato',
+      Standard: "III standard",
+      Phone_Number: 123,
+      ERP_Number: 5454,
+      Status: 'Active ',
+      Details: 'View'
 
-        },
-        {
-            Student_Name : 'deepath',
-            id:1,
-            Type: 'Minato',
-            Standard: "III standard",
-            Phone_Number: 123,
-            ERP_Number: 5454,
-            Status : 'Active ',
-            Details : 'View' 
+    },
+    {
+      Student_Name: 'deepath',
+      id: 1,
+      Type: 'Minato',
+      Standard: "III standard",
+      Phone_Number: 123,
+      ERP_Number: 5454,
+      Status: 'Active ',
+      Details: 'View'
 
-        },
-        {
-            Student_Name : 'deepath',
-            id:1,
-            Type: 'Minato',
-            Standard: "III standard",
-            Phone_Number: 123,
-            ERP_Number: 5454,
-            Status : 'Active ',
-            Details : 'View' 
+    },
+    {
+      Student_Name: 'deepath',
+      id: 1,
+      Type: 'Minato',
+      Standard: "III standard",
+      Phone_Number: 123,
+      ERP_Number: 5454,
+      Status: 'Active ',
+      Details: 'View'
 
-        },
-        {
-            Student_Name : 'deepath',
-            id:1,
-            Type: 'Minato',
-            Standard: "III standard",
-            Phone_Number: 123,
-            ERP_Number: 5454,
-            Status : 'Active ',
-            Details : 'View' 
+    },
+    {
+      Student_Name: 'deepath',
+      id: 1,
+      Type: 'Minato',
+      Standard: "III standard",
+      Phone_Number: 123,
+      ERP_Number: 5454,
+      Status: 'Active ',
+      Details: 'View'
 
-        },   {
-            Student_Name : 'deepath',
-            id:1,
-            Type: 'Minato',
-            Standard: "III standard",
-            Phone_Number: 123,
-            ERP_Number: 5454,
-            Status : 'Active ',
-            Details : 'View' 
+    }, {
+      Student_Name: 'deepath',
+      id: 1,
+      Type: 'Minato',
+      Standard: "III standard",
+      Phone_Number: 123,
+      ERP_Number: 5454,
+      Status: 'Active ',
+      Details: 'View'
 
-        },
+    },
 
-    ];
-const headings = Object.keys(data[0]);
+  ];
+  const headings = Object.keys(data[0]);
   return (
- 
+    <div className='w-1/2 m-auto'>
+      <TableContainer component={Paper} className='rounded-xl overflow-hidden'>
+        <Table sx={{ minWidth: 200 }} aria-label="simple table">
+          <TableHead className='bg-blue-100'>
+            <TableRow>
+              {headings.map((heading) => (
+                heading !== 'id' ? (
+                  <TableCell key={heading}>{heading}</TableCell>
+                ) :
+                  null
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((value) => (
+              <TableRow key={value.id} >
 
-      <table className='m-auto my-10 rounded-xl'>
-        <thead>
-        <tr className=' rounded-tl-lg '>
-          {headings.map((heading)=>(
-            heading !== 'id' ? (
-            <th key={heading} className='px-5 bg-cBlue border border-solid border-borHead border-1 rounded-t-20'>
-                {heading}
-            </th>
-            ) :
-            null
-          ))}
-        </tr>
-        </thead>
-        <tbody>
-        {data.map((value) => (
-          <tr key={value.id}>
-            {headings.map((heading) => (
-                heading === 'id' ? (
+                {headings.map((heading) => (
+                  heading === 'id' ? (
                     null
-                  ) : heading ==="Details" ? (
-                    <td key={heading} className='px-5 text-center border border-solid  border-1 '>
-                       <button> {value[heading]} </button> 
-                    </td>
+                  ) : heading === "Details" ? (
+                    <TableCell key={heading} align="left"><button> {value[heading]} </button></TableCell>
 
-                  ):
-                  
-                  (
-                    <td key={heading} className='px-5 text-center border border-solid  border-1'>{value[heading]}</td>
+                  ) :
 
-                  )
+                    (
+                      <TableCell key={heading} align="left"><button> {value[heading]}</button></TableCell>
+                    )
+                ))}
+              </TableRow>
             ))}
-          </tr>
-        ))}
-      </tbody>
-      </table>
- 
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   )
 }
 
