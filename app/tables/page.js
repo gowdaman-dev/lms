5b8607afb39c5a1e8065d49584e468faa77841c9
gowdaman-dev <damangowdaman@gmail.com>
2024-01-16 11:03:34 +0530

    deepath

--- app/tables/page.js
+++ app/tables/page.js
@@ -2,11 +2,10 @@ import React from 'react'
 import Admin from '@/components/dashboard/Admin'
 const table = () => {
   return (
-   
       <Admin />
-     
-    
+      // new branch
   )
 }
 
 export default table
+ls
