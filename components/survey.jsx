f67a797e0f584b3740dc4b9b8461999793e7e6f4
gowdaman-dev <damangowdaman@gmail.com>
2024-01-16 22:04:37 +0530

    layout

--- components/survey.jsx
+++ components/survey.jsx
@@ -7,19 +7,21 @@ const Survey = () => {
   return (
     <ul className="w-screen mt-10">
       <div className="w-2/3 mx-auto  flex  sm:flex-row flex-col lg:flex-row justify-between items-center ">
-      <p className="sm:hidden py-2 flex text-2xl text-blue-500">{title}</p>
-        <div className="flex gap-10">
+        <p className="sm:hidden py-2 flex text-2xl text-blue-500">{title}</p>
+        <div className="flex gap-10 text-center bg-blue-50 py-4 px-10 rounded-lg">
           <li>
             <FaChartColumn className="text-hBlue mt-2 text-4xl " />
+            <FaChartColumn className="text-hBlue mt-2 " />{" "}
           </li>
           <div>
             <li className="">Today Logins</li>
             <li>{today}</li>
           </div>
         </div>
-        <li className="">{title}</li>
-        <div className="flex gap-10 text-center">
+        <li className="sm:flex hidden text-3xl text-blue-500">{title}</li>
+        <div className="flex gap-10 text-center bg-blue-50 py-4 px-10 rounded-lg">
           <li>
+            <FaChartLine className="text-hBlue mt-2 text-4xl" />
             <FaChartLine className="text-hBlue mt-2 text-4xl" />
           </li>
           <div>
