73eb7a08f6227fad6bebbdfb6d4a2116678b27ae
deepath15 <deepath1506@gmail.com>
2024-01-16 21:40:11 +0530

    suvey

--- components/survey.jsx
+++ components/survey.jsx
@@ -6,26 +6,18 @@ const total = 300;
 const Survey = () => {
   return (
     <ul className="w-screen mt-10">
-      <div className="w-2/3 mx-auto  flex  sm:flex-row flex-col lg:flex-row justify-between items-center ">
-        <p className="sm:hidden py-2 flex text-2xl text-blue-500">{title}</p>
-        <div className="flex gap-10 text-center bg-blue-50 py-4 px-10 rounded-lg">
+      <div className="w-2/3 mx-auto  flex  flex-wrap lg:flex-row justify-between ">
+        <div className="flex gap-10">
           <li>
-            <FaChartColumn className="text-hBlue mt-2 text-4xl " />
-<<<<<<< HEAD
-            <FaChartColumn className="text-hBlue mt-2 " />{" "}
-=======
->>>>>>> 716eb06 (layout)
+            <FaChartColumn className="text-hBlue mt-2" />{" "}
           </li>
-          <div>
-            <li className="">Today Logins</li>
-            <li>{today}</li>
-          </div>
+          <li className="">Today Logins</li>
+          <li>{today}</li>
         </div>
-        <li className="sm:flex hidden text-3xl text-blue-500">{title}</li>
-        <div className="flex gap-10 text-center bg-blue-50 py-4 px-10 rounded-lg">
+        <li className="item sm:order-first">{title}</li>
+        <div className="flex gap-10 text-center">
           <li>
-            <FaChartLine className="text-hBlue mt-2 text-4xl" />
-            <FaChartLine className="text-hBlue mt-2 text-4xl" />
+            <FaChartLine />
           </li>
           <div>
             <li>Total Students</li>
