d5eda0e9a913b68f35b1458ce4ea344bc9c1218c
deepath15 <deepath1506@gmail.com>
2024-01-16 21:40:11 +0530

    suvey

---
+++ components/survey.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { FaChartColumn, FaChartLine } from "react-icons/fa6";
+const today = 144;
+const title = "Welcome";
+const total = 300;
+const Survey = () => {
+  return (
+    <ul className="w-screen mt-10">
+      <div className="w-2/3 mx-auto  flex  flex-wrap lg:flex-row justify-between ">
+        <div className="flex gap-10">
+          <li>
+            <FaChartColumn className="text-hBlue mt-2" />{" "}
+          </li>
+          <li className="">Today Logins</li>
+          <li>{today}</li>
+        </div>
+        <li className="item sm:order-first">{title}</li>
+        <div className="flex gap-10 text-center">
+          <li>
+            <FaChartLine />
+          </li>
+          <div>
+            <li>Total Students</li>
+            <li>{total}</li>
+          </div>
+        </div>
+      </div>
+    </ul>
+  );
+};
+
+export default Survey;
