628b39c3de5d9c0bde9f15146bb73329a6f827cc
gowdaman-dev <damangowdaman@gmail.com>
2024-01-17 00:07:29 +0530

    new

--- components/navbar/navlinks/StudentNav.jsx
+++ components/navbar/navlinks/StudentNav.jsx
@@ -16,13 +16,13 @@ function StudentNav() {
                         className={`text-gray-800 w-[90%] py-3 px-4 flex items-center gap-2 text-xl rounded-r-full ${(crtpath == item.path)?"shadow-[1px_1px_0px_0px] shadow-gray-500/[.5] bg-white":""} `}
                     >
                         {item.icon}
-                        <p className='text-sm'>
+                        <p className='text-sm font-light'>
                             {item.lable}
                         </p>
                     </Link>
                 })
             }
-            <button className='px-4 text-xl text-red-400 flex items-center justify-start gap-2'><FaRightFromBracket/><p className='text-sm'>Logout</p></button>
+            <button className='px-4 text-xl text-red-400 flex items-center justify-start gap-2'><FaRightFromBracket/><p className='text-sm font-light'>Logout</p></button>
         </>
     )
 }
