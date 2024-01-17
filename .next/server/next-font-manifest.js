2bbc50da496e8d263a71d357dbdafa5cc36e2aa3
deepath15 <deepath1506@gmail.com>
2024-01-17 20:45:14 +0530

    removed .next

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/deepath/Documents/Deepath/vscode/web/node/demo/LMS-edulearn/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"],\"/home/deepath/Documents/Deepath/vscode/web/node/demo/LMS-edulearn/app/dashboard/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"SCpK+A+UG2BSiCdgg8VUsL4IjTjCmqiTGfq7ObGGvd0=\"\n}"
