128b4c203d3c1fcf13787c159b44c53747125542
jahangeer7704 <jahangeer7704@gmail.com>
2024-01-17 13:59:11 +0530

    chart

--- components/dashboard/Admin.jsx
+++ components/dashboard/Admin.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import Table from '@/components/Table'
 import AdminBarMonth from '../charts/Admin/AdminBarMonth'
 const Admin = () => {
-  const std = 'VII'
+  
   return (
     <div className='h-screen'>
       <div className='h-1/6'>Header</div>
-      <div className='flex h-2/6 w-screen justify-center gap-14 '>
-        <section className='h-full w-1/3 bg-[#F2F5F9]'></section>{' '}
-        <section className='h-full w-1/3 bg-[#F2F5F9]'></section>
+      <div className='flex h-2/6 w-screen flex-col sm:flex-row justify-center gap-x-14 gap-y-5 items-center '>
+        <section className='h-full flex flex-col justify-center items-center w-[80%] sm:w-2/6 bg-[#F2F5F9] rounded-lg'>
+          
+          <AdminBarMonth/>
+          </section>
+          <section className='h-full flex flex-col justify-center items-center w-[80%] sm:w-2/6 bg-[#F2F5F9] rounded-lg'>
+          
+          <AdminBarMonth/>
+          </section>
       </div>
+      <div className='mt-20  h-2/6'>
+
       <Table />
       </div>
     </div>
