b42ad68ea74b4c71e056a943d340eff06f4a429f
jahangeer7704 <jahangeer7704@gmail.com>
2024-01-17 14:28:45 +0530

    jk

--- components/survey.jsx
+++ components/survey.jsx
@@ -11,7 +11,10 @@ const Survey = () => {
         <div className="flex gap-10 text-center bg-blue-50 py-4 px-10 rounded-lg">
           <li>
             <FaChartColumn className="text-hBlue mt-2 text-4xl " />
+<<<<<<< HEAD
             <FaChartColumn className="text-hBlue mt-2 " />{" "}
+=======
+>>>>>>> 716eb06 (layout)
           </li>
           <div>
             <li className="">Today Logins</li>
