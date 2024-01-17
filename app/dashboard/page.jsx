27b8ed97c9437b686012164936f6d9a3c34db3c6
gowdaman-dev <damangowdaman@gmail.com>
2024-01-17 20:04:01 +0530

    new

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,5 @@
 import Admin from '@/components/dashboard/Admin'
 import React from 'react'
-import Admin from '@/components/dashboard/Admin'
 function page() {
   return (
     <div className=' w-full px-10 py-10'>
