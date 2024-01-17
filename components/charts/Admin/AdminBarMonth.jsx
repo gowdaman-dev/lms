918f77e20bc7a7334159c9afd77ae008e3520009
jahangeer7704 <jahangeer7704@gmail.com>
2024-01-17 14:39:44 +0530

    jk

---
+++ components/charts/Admin/AdminBarMonth.jsx
@@ -0,0 +1,49 @@
+"use client"
+import React from 'react'
+import { Bar} from 'react-chartjs-2'
+import { Chart as chartjs } from 'chart.js/auto'
+
+function AdminBarMonth() {
+  
+  const chart={
+    labels:["jan","Feb","Mar","Apr","May","Jun","Jul"],
+    datasets:[{
+      label:"Monthly Analysis",
+      data:[300,400,700,1000,200,100,700],
+   
+    borderRadius:10,
+    bodyColor:"red"
+    }]
+    
+
+  }
+  const options = {
+    scales: {
+      x: {
+        ticks: {
+          color: 'blue', // Change x-axis label color
+        },
+      },
+      y: {
+        ticks: {
+          color: 'blue',
+          // Change y-axis label color
+        },
+        beginAtZero: true,
+      },
+    },
+    plugins: {
+      legend: {
+        labels: {
+          color: 'black', // Change legend text color
+         
+        },
+      },
+    },
+  };
+  return (
+   <Bar data={chart} options={options} />
+  )
+}
+
+export default AdminBarMonth
