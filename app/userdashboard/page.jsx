ddfb203ef6496e84c1135da191a9d4b540851714
gowdaman-dev <damangowdaman@gmail.com>
2024-01-18 22:33:03 +0530

    hari

---
+++ app/userdashboard/page.jsx
@@ -0,0 +1,12 @@
+import Admin from '@/components/dashboard/Admin'
+import Slider from '@/components/dashboard/userdashboard/Slider'
+import React from 'react'
+function page() {
+  return (
+    <div className='w-full h-fit flex-col items-center'>
+      <Slider/>
+    </div>
+  )
+}
+
+export default page
